fb4ff61f0efba3bc313be42b9216c506e83c2e4c
necokriel <necokriel@gmail.com>
2022-05-09 13:09:42 +1000

    remove demo files

--- scripts/script_download_plots.py
+++ scripts/script_download_plots.py
@@ -21,10 +21,12 @@ plt.ioff()
 plt.switch_backend("agg") # use a non-interactive plotting backend
 
 
-BASEPATH_MAC  = "/Users/dukekriel/Documents/Studies/TurbulentDynamo/data/"
-BASEPATH_GADI = "/scratch/ek9/nk7952/"
-SONIC_REGIME  = "super_sonic"
-FILENAME      = "*_check.png"
+BASEPATH_MAC    = "/Users/dukekriel/Documents/Studies/TurbulentDynamo/data/"
+BASEPATH_GADI   = "/scratch/ek9/nk7952/"
+SONIC_REGIME    = "super_sonic"
+FILENAME        = "*_check.png"
+BOOL_GET_PLOTS  = True
+BOOL_GET_VIDEOS = False
 ## ###############################################################
 ## DEFINE MAIN PROGRAM
 ## ###############################################################
@@ -34,8 +36,8 @@ def main():
     ]: # "Re10", "Re500", "Rm3000", "keta"
 
     for sim_res in [
-        "18", "36", "72", "144", "288"
-      ]: # "72", "144", "288", "576"
+        "72", "144", "288", "576"
+      ]: # "18", "36", "72", "144", "288", "576"
 
       ## ################################
       ## CREATE FILEPATH TO FOLDER ON MAC
@@ -57,29 +59,31 @@ def main():
       ## ##############################
       ## DOWNLOAD FIT FIGURES FROM GADI
       ## ##############################
-      filepath_gadi_figures = createFilepath([
-        "gadi:"+BASEPATH_GADI, suite_folder, sim_res, SONIC_REGIME, "vis_folder"
-      ])
-      print("Downloading from:", filepath_gadi_figures)
-      ## download plots checking fits
-      os.system("scp {}/{} {}/.".format(
-        filepath_gadi_figures,
-        FILENAME,
-        filepath_mac_figures
-      ))
+      if BOOL_GET_PLOTS:
+        filepath_gadi_figures = createFilepath([
+          "gadi:"+BASEPATH_GADI, suite_folder, sim_res, SONIC_REGIME, "vis_folder"
+        ])
+        print("Downloading from:", filepath_gadi_figures)
+        ## download plots checking fits
+        os.system("scp {}/{} {}/.".format(
+          filepath_gadi_figures,
+          FILENAME,
+          filepath_mac_figures
+        ))
 
       ## #############################
       ## DOWNLOAD FIT VIDEOS FROM GADI
       ## #############################
-      filepath_gadi_videos = createFilepath([
-          "gadi:"+BASEPATH_GADI, suite_folder, sim_res, SONIC_REGIME, "vis_folder"
-      ])
-      print("Downloading from:", filepath_gadi_videos)
-      ## download plots + animations
-      os.system("scp {}/*.mp4 {}/.".format(
-        filepath_gadi_videos,
-        filepath_mac_figures
-      ))
+      if BOOL_GET_VIDEOS:
+        filepath_gadi_videos = createFilepath([
+            "gadi:"+BASEPATH_GADI, suite_folder, sim_res, SONIC_REGIME, "vis_folder"
+        ])
+        print("Downloading from:", filepath_gadi_videos)
+        ## download plots + animations
+        os.system("scp {}/*.mp4 {}/.".format(
+          filepath_gadi_videos,
+          filepath_mac_figures
+        ))
       print(" ")
 
 
