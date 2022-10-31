2a6e7b11e6460063360ccbbdaa58c415fd1d836a
neco kriel <necokriel@gmail.com>
2022-10-31 19:33:16 +1100

    simplify files for merge

--- TheMHDPackage/ThePlottingModule/__init__.py
+++
@@ -1,7 +0,0 @@
-from .TheMatplotlibStyler import *
-
-## the list of module names (in the PACKAGE) that should be imported when "from PACKAGE import *"" is encountered
-__all__ = [
-  "PlotFuncs",
-  "PlotSpectra",
-]
